2d4f76296b512b72c186b08a9d7ffc7ad8c3b00b
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-06-23 11:09:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaxSpree::Application.config.secret_token = '026d348044bf08cc5b4a1e66209c1766762a9100df22d5c9581cbec5ee77360456fe212a616a8bcfb6cd274b9e266c46cf6f7a6d2770114574fbdde7553528d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MaxSpree::Application.config.session_store :cookie_store, key: '_max-spree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MaxSpree::Application.config.session_store :active_record_store
